c773e43356eedaae2b225619f313fe4396fa493f
Malik22G <malik.basit3690@gmail.com>
2024-05-20 04:17:26 +0200

    Dark Mode Fix and adding Transcript

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
     const hour = new Date().getHours();
-    if (hour >= 18 || hour < 6) {
+    if (!(hour >= 18 || hour < 6)) {
         document.body.classList.add('dark-mode');
     }
 });
